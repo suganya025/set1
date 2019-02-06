2b7cfd463d55fd0ab1108011204a6f8f9cc7d05c
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-06 16:15:07 +0530

    Update prog5.java

--- prog5.java
+++ prog5.java
@@ -6,20 +6,20 @@ class largestnum
 	public static void main (String[] args)   
 	{    
 	Scanner ob = new Scanner(System.in);
-  int a= ob.nextInt();    
+  	int a= ob.nextInt();    
 	int b= ob.nextInt();
 	int c= ob.nextInt();
 		if((a>b)&&(a>c))    		
-    { 
-      System.out.println(+a);  
+    		{ 
+     			 System.out.println(+a);  
 		}   
 		else if((b>a)&&(b>c))   		
-    {  
+    		{  
 			System.out.println(+b);  	        
-    }  
+   		 }  
 		else   
-    {    
+    		{    
 			System.out.println(+c);   	        
-    }  
+    		}  
 	}
 }
