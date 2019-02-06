a27b924aeb495e5100a0647a06bbc8a1a25c26bc
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-06 16:14:02 +0530

    Update prog5.java

--- prog5.java
+++ prog5.java
@@ -3,25 +3,23 @@ import java.lang.*;
 import java.io.*;
 class largestnum
 {  
-public static void main (String[] args)   
-{
-Scanner ob = new Scanner(System.in);    
-System.out.println("INPUT");    
-int a= ob.nextInt();    
-int b= ob.nextInt();
-int c= ob.nextInt();
-System.out.println("OUTPUT");    
-if((a>b)&&(a>c))    
-{    
-System.out.println(+a);    
-}    
-else if((b>a)&&(b>c))    
-{    
-System.out.println(+b);    
-}    
-else     
-{    
-System.out.println(+c);    
-}  
-}
+	public static void main (String[] args)   
+	{    
+	Scanner ob = new Scanner(System.in);
+  int a= ob.nextInt();    
+	int b= ob.nextInt();
+	int c= ob.nextInt();
+		if((a>b)&&(a>c))    		
+    { 
+      System.out.println(+a);  
+		}   
+		else if((b>a)&&(b>c))   		
+    {  
+			System.out.println(+b);  	        
+    }  
+		else   
+    {    
+			System.out.println(+c);   	        
+    }  
+	}
 }
