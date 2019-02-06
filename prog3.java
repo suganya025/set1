1d1eb1df1b91c1a782ab52b78ece27622fca0611
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-06 15:49:46 +0530

    Create prog3.java

---
+++ prog3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+class vowel
+{  
+public static void main (String[] args)  
+{    	
+Scanner ob = new Scanner(System.in);    
+	char ch= ob.next().charAt(0);
+  if(ch=='a'||ch =='e'||ch=='i'||ch=='o'||ch=='u')   
+	{	
+	System.out.println("Vowel");  
+	}
+	else if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+	{
+		System.out.println("Consonant");
+	}
+	else
+	{		
+	System.out.println("invalid");	
+  }
+}
+
+}
