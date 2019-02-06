23d604acc3f58eabe43e18054a8f86fd6cab4347
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-06 14:34:58 +0530

    Update prog2.java

--- prog2.java
+++ prog2.java
@@ -6,14 +6,21 @@ class oddoreven
 public static void main (String[] args)   
 {    
 Scanner ob = new Scanner(System.in);     
-int num= ob.nextInt();    
-if((num%2)==0)    
-{    
-System.out.println("even");    
-}    
-else     
+int num= ob.nextInt(); 
+if(num>=0)
 {
-System.out.println("odd");  
-}  
+		if((num%2)==0)    
+		{    
+		System.out.println("Even");    
+		}    
+		else     
+		{
+		System.out.println("Odd");  
+		}
+}
+else
+{
+	System.out.println("invalid");
+}
 }
 }
