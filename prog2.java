689716f0346cd70496689ba32ed332af8b308936
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-06 14:24:46 +0530

    Update prog2.java

--- prog2.java
+++ prog2.java
@@ -5,10 +5,8 @@ class oddoreven
 {  
 public static void main (String[] args)   
 {    
-Scanner ob = new Scanner(System.in);    
-System.out.println("INPUT");    
+Scanner ob = new Scanner(System.in);     
 int num= ob.nextInt();    
-System.out.println("OUTPUT");    
 if((num%2)==0)    
 {    
 System.out.println("even");    
