3e1e25b992205bda71e2d9f2ae84b4c097c9cc62
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-09 22:06:37 +0530

    Update prog7.java

--- prog7.java
+++ prog7.java
@@ -7,14 +7,14 @@ public static void main (String[] args)
 	{    
 	Scanner ob = new Scanner(System.in);
   	int n= ob.nextInt();   
-		 if(n!=0)
-		 {
-		 for(int i=1;i<=n;i++)    		
+		if(n!=0)
+		{
+		for(int i=1;i<=n;i++)    		
     		{ 
      			 System.out.println("Hello");  
 		}  
 		}
-				else   
+		else   
     		{    
 		   	        
     		}  
