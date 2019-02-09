0bf6363e93b792dda8a110179f4bba107da9569e
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-09 22:05:39 +0530

    Create prog7.java

---
+++ prog7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ninput
+{
+public static void main (String[] args)   
+	{    
+	Scanner ob = new Scanner(System.in);
+  	int n= ob.nextInt();   
+		 if(n!=0)
+		 {
+		 for(int i=1;i<=n;i++)    		
+    		{ 
+     			 System.out.println("Hello");  
+		}  
+		}
+				else   
+    		{    
+		   	        
+    		}  
+	} 
+}
