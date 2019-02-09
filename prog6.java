341e6865d3b15b71f09971c403b6c4ea136d672f
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-09 21:48:17 +0530

    Create prog6.java

---
+++ prog6.java
@@ -0,0 +1,19 @@
+import java.util.scanner;
+import java.lang.*;
+import java.io.*;
+class leafyr
+{
+public static void main (String[] args)   
+	{    
+	Scanner ob = new Scanner(System.in);
+  	int a= ob.nextInt();   
+		 if((a%4)==0)    		
+    		{ 
+     			 System.out.println("yes");  
+		}   
+				else   
+    		{    
+			System.out.println("no");   	        
+    		}  
+	} 
+}
