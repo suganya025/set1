e976a51558cdc7c4b24ee3ebb6164f92918b4546
suganya025 <47377302+suganya025@users.noreply.github.com>
2019-02-09 22:16:38 +0530

    Create prog8.java

---
+++ prog8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class sumofn
+{
+public static void main (String[] args)   
+	{    
+	Scanner ob = new Scanner(System.in);
+  	int n= ob.nextInt(); 
+  	int a=0;
+		 		 for(int i=0;i<=n;i++)    		
+    		{ 
+    			 a=a+i;
+    		}
+     			 System.out.println(+a);  
+			} 
+}
